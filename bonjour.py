65c2a7795d272f43b2ceff00ea63d3b039bfcf22
Antoine Coroller <330083333+CorollerAntoine@users.noreply.github.com>
2026-09-16 22:42:24 +0200

    Premier programme : message de bienvenue

---
+++ bonjour.py
@@ -0,0 +1,5 @@
+prenom = input("Quel est ton nom ? ")
+heures_semaine = int(input("Combien d'heures par semaine comptes-tu coder ? "))
+heures_an = heures_semaine*52
+phrase = f"Bienvenue {prenom} ! A ce rythme, tu coderas {heures_an} heures d'ici un an."
+print(phrase)
