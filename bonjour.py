80788f8431b77fa18fa8d88ad4b9b37b5e099afc
Antoine Coroller <330083333+CorollerAntoine@users.noreply.github.com>
2026-09-16 23:12:11 +0200

    Gestion des erreurs de saisie avec try/except

--- bonjour.py
+++ bonjour.py
@@ -1,8 +1,10 @@
 prenom = input("Quel est ton nom ? ")
-try:
-    heures_semaine = int(input("Combien d'heures par semaine comptes-tu coder ? "))
-except ValueError:
-    print("Oups, il faut écrire un nombre en chiffres !")
+while True:
+    try:
+        heures_semaine = int(input("Combien d'heures par semaine comptes-tu coder ? "))
+        break
+    except ValueError:
+        print("Oups, il faut écrire un nombre en chiffres !")
 heures_an = heures_semaine*52
 phrase = f"Bienvenue {prenom} ! A ce rythme, tu coderas {heures_an} heures d'ici un an."
 print(phrase)
