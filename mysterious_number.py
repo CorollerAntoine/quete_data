ec2d1138ba47eccd0e35067c4515761951f40523
Antoine Coroller <330083333+CorollerAntoine@users.noreply.github.com>
2026-09-22 21:43:40 +0200

    Ask a mysterious number between 1 and 100

---
+++ mysterious_number.py
@@ -0,0 +1,32 @@
+##The code chooses a mysterious number and asks the user to guess it.
+##At the end, the program reveals the number of tries it took the user to guess the number.
+##seven tries maximum
+
+import random
+
+mysterious_number = random.randint(1, 100)
+tries = 0 
+
+while tries < 7 : 
+    try:
+        guess = int(input("Take a guess from 1 to 100 : "))
+        if guess < 1 or guess > 100:
+            print("The number must be between 1 and 100. ")
+            continue
+    except ValueError:
+        print("Oops, you must enter a number in digits!")
+        break
+
+    tries+=1
+
+    if guess == mysterious_number:
+        print(f"Congratulations! You guessed the mysterious number in {tries} tries.")
+        break
+    elif guess < mysterious_number:
+        print("Too low!")
+    elif guess > mysterious_number:
+        print("Too high!")
+    if tries == 7:
+        print(f"Sorry, you've used all 7 tries. The mysterious number was {mysterious_number}.")
+        break
+
